7220eb3387796dfb78220b3481565dd7e7238efd
Sara Jensen and Sheena Nickerson <sara@>
2016-09-06 16:43:51 -0700

    ping-pong in java is tested in gradle

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,14 @@
+import java.io.Console;
+import java.util.List;
+
 public class App {
   public static void main(String[] args) {
-
-    System.out.println("I'm an example application!");
-
+    Console myConsole = System.console();
+    System.out.println("Enter a number:");
+    String stringUserNumber = myConsole.readLine();
+    int intUserNumber = Integer.parseInt(stringUserNumber);
+    PingPong pingPong = new PingPong();
+    List<Object> pingPongResult = pingPong.runPingPong(intUserNumber);
+    System.out.println(pingPongResult);
   }
 }
